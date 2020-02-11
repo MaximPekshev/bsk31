875903b2cf1693bd21a28e920560f9941cea7d37
MaximPekshev <mp198769@gmail.com>
2020-02-11 17:37:08 +0300

    added models of object + templates

--- baseapp/views.py
+++ baseapp/views.py
@@ -1,9 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from objectapp.models import Build_object
+from objectapp.models import Picture
 
-# Create your views here.
+
+class Item(object):
+	
+	build_object 	= Build_object
+	image 			= Picture
+
+
 def show_index(request):
-	return render(request, 'baseapp/index.html')
+
+	build_objects = Build_object.objects.all().order_by('object_top')[:5]
+
+	table = []
+
+	for obj in build_objects:
+
+		item = Item()
+		
+		item.build_object = obj
+		
+		images = Picture.objects.get(build_object=obj, main_image=True)
+		
+		item.image = images 
+		 	
+		table.append(item)
+
+	template_name = 'baseapp/index.html'
+
+	context = {
+
+		'build_objects': table,
+	}
+
+	return render(request, template_name, context)	
+
 
 def show_about_us(request):
 	return render(request, 'baseapp/about_us.html')
