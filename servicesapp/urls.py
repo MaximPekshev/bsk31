0a557c417d454fcd07bb3c1853995dfd01c0c861
MaximPekshev <mp198769@gmail.com>
2020-03-19 13:06:54 +0300

    version without services page

--- servicesapp/urls.py
+++ servicesapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import show_service_objects
+
+from .views import show_services
 from .views import show_service_object
 
 urlpatterns = [
 
-	path('', 				show_service_objects, name='show_service_objects'),
+	# path('', 				show_services, name='show_services'),
 	path('<str:slug>/', 	show_service_object, name='show_service_object'),
-
+	
 			]
--- servicesapp/views.py
+++ servicesapp/views.py
@@ -4,7 +4,7 @@ from .models import Service_object
 from .models import Service_picture
 
 
-def show_service_objects(request):
+def show_services(request):
 
 	service_objects = Service_object.objects.all()
 	s_objects 		= Service_object.objects.all().order_by('-object_top')
@@ -12,7 +12,8 @@ def show_service_objects(request):
 	template_name = 'servicesapp/services.html'
 
 	context = {
-		'service_objects': service_objects, 's_objects': s_objects,
+		'service_objects': service_objects, 
+		's_objects' : s_objects,
 	}
 
 	return render(request, template_name, context)
@@ -31,4 +32,4 @@ def show_service_object(request, slug):
 		'service_objects': service_objects, 's_object':s_object, 's_picture': s_picture,
 	}
 
-	return render(request, template_name, context)	
+	return render(request, template_name, context)
