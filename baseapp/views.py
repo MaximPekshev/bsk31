be12775157ea1f74d25ac1f86d420fa613677cfa
MaximPekshev <mp198769@gmail.com>
2020-03-26 16:54:30 +0300

    certapp

--- baseapp/views.py
+++ baseapp/views.py
@@ -4,11 +4,18 @@ from objectapp.models import Build_object
 from objectapp.models import Picture
 
 from servicesapp.models import Service_object
+from certapp.models import Cert_object
+from certapp.models import Picture as Cert_picture
 
 class Item(object):
 	
 	build_object 	= Build_object
 	image 			= Picture
+
+class Cert_item(object):
+	
+	cert_object 	= Cert_object
+	cert_picture 	= Cert_picture
 
 
 def show_index(request):
@@ -63,3 +70,28 @@ def show_under_construct(request):
 		'service_objects': service_objects,
 	}
 	return render(request, 'baseapp/under_construct.html', context)
+
+
+def show_certificates(request):
+
+	service_objects = Service_object.objects.all()
+
+	cert_objects = Cert_object.objects.all()
+
+	table = []
+
+	for obj in cert_objects:
+
+		item = Cert_item()
+		
+		item.cert_object = obj
+
+		item.cert_picture = Cert_picture.objects.filter(cert_object=obj).first()	 
+		 	
+		table.append(item)
+
+	context = {
+
+		'service_objects': service_objects, 'cert_objects': table,
+	}
+	return render(request, 'baseapp/certificates.html', context)
--- bsk31/settings.py
+++ bsk31/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'workingdayapp',
     'rentapp',
     'servicesapp',
+    'certapp',
 ]
 
 MIDDLEWARE = [
